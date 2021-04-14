b624bc798be165dd7a8d40a5d3bae31dd0478b79
Micledson Carlos <micledson14@gmail.com>
2021-04-14 13:17:08 -0300

    add exceção SaldoInsuficiente

--- projetojava/src/test/java/projetojava/BancoTeste.java
+++ projetojava/src/test/java/projetojava/BancoTeste.java
@@ -1,9 +1,12 @@
 package projetojava;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import projetojava.Banco.SaldoInsuficiente;
 
 public class BancoTeste {
     Banco banco;
@@ -14,7 +17,9 @@ public class BancoTeste {
     }
 
     @Test
-    public void testeSaque() {
-        assertEquals(banco.getSaldo() - 100, banco.saque(100));
+    public void testeSaque() throws SaldoInsuficiente {
+        assertEquals(banco.getSaldo() - 50, banco.saque(50));
+        assertEquals(banco.getSaldo() - 50, banco.saque(50));
+        assertThrows(Banco.SaldoInsuficiente.class, () -> banco.saque(200));
     }
 }
